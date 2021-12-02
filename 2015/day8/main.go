c3c1ecc2cd09e1e2a0879541c83ee262d90c53df
Riley Thompson <riley.thompson@flybits.com>
2021-12-02 17:56:21 -0500

    solved day 8 2015

---
+++ 2015/day8/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func part1(strs []string) int {
+	// solved with vscode regex searching
+	// to count and substract characters
+	return 0
+}
+
+func part2(strs []string) int {
+	// solved with vscode regex searching to
+	// count and add up additional characters
+	// ans: 2117
+	return 0
+}
+
+func main() {
+	level, fileName := getArgs()
+	if level == "" || fileName == "" {
+		return
+	}
+
+	file, _ := os.Open(fileName)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	strs := []string{}
+	nums := []int{}
+	for scanner.Scan() {
+		str := scanner.Text()
+		strs = append(strs, str)
+		num, _ := strconv.Atoi(str)
+		nums = append(nums, num)
+	}
+
+	if level == "1" {
+		fmt.Println(part1(strs))
+	} else if level == "2" {
+		fmt.Println(part2(strs))
+	}
+}
+
+func getArgs() (string, string) {
+	args := os.Args[1:]
+	var (
+		level    = "1"
+		fileName = "input.txt"
+	)
+	for i, arg := range args {
+		if i == 0 && (level == "1" || level == "2") {
+			level = arg
+		} else if i == 0 {
+			fmt.Printf("Level must be 1 or 2, got %s\n", arg)
+			return "", ""
+		} else if i == 1 {
+			fileName = arg
+		}
+	}
+	return level, fileName
+}
