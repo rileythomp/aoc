889fb11a685c2a7f66e18e7606f252935f2b7bd0
Riley Thompson <riley.thompson@flybits.com>
2021-12-02 09:28:07 -0500

    finish 2015 day 7

--- 2015/day7/main.go
+++ 2015/day7/main.go
@@ -8,6 +8,34 @@ import (
 	"strings"
 )
 
+type Stack []string
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) Pop() string {
+	if s.IsEmpty() {
+		return ""
+	}
+	i := len(*s) - 1
+	str := (*s)[i]
+	*s = (*s)[:i]
+	return str
+}
+
+func (s *Stack) Top() string {
+	if s.IsEmpty() {
+		return ""
+	}
+	str := (*s)[len(*s)-1]
+	return str
+}
+
 type Wire struct {
 	Val      uint16
 	Gate     string
@@ -15,64 +43,106 @@ type Wire struct {
 }
 
 func processGatePart(gatePart string, wires map[string]Wire) (uint16, bool) {
-	// if its a number
 	if num, err := strconv.Atoi(gatePart); err == nil {
 		return uint16(num), true
 	}
-
-	// its a wire, check map
 	if wire, ok := wires[gatePart]; ok {
 		return wire.Val, true
 	}
 	return 0, false
 }
 
-func processGate(gate string, wires map[string]Wire) (uint16, bool) {
+func processGate(gate string, wires map[string]Wire) (uint16, string, string) {
 	// X
-	if num, err := strconv.Atoi(gate); err == nil {
-		return uint16(num), true
+	if !strings.Contains(gate, " ") {
+		val, ok := processGatePart(gate, wires)
+		if ok {
+			return val, "", ""
+		}
+		return 0, gate, ""
 	}
 	gateParts := strings.Split(gate, " ")
 	// NOT X
 	if strings.Contains(gate, "NOT") {
 		val, ok := processGatePart(gateParts[1], wires)
 		if ok {
-			return ^val, true
+			return ^val, "", ""
 		}
+		return 0, gateParts[1], ""
 	}
-	left, lOk := processGatePart(gateParts[0], wires)
-	right, rOk := processGatePart(gateParts[2], wires)
+	if len(gateParts) < 3 {
+		fmt.Printf("Unexpected gate: %s, %s\n", gate, gateParts)
+		return 0, "", ""
+	}
+	l, lOk := processGatePart(gateParts[0], wires)
+	r, rOk := processGatePart(gateParts[2], wires)
 	if strings.Contains(gate, "AND") && lOk && rOk {
-		return (uint16(left) & uint16(right)), true
+		return l & r, "", ""
 	}
 	if strings.Contains(gate, "OR") && lOk && rOk {
-		return (uint16(left) | uint16(right)), true
+		return l | r, "", ""
 	}
 	if strings.Contains(gate, "LSHIFT") && lOk && rOk {
-		return (uint16(left) << uint16(right)), true
+		return l << r, "", ""
 	}
 	if strings.Contains(gate, "RSHIFT") && rOk && lOk {
-		return (uint16(left) >> uint16(right)), true
+		return l >> r, "", ""
 	}
-	return 0, false
+	lw, rw := "", ""
+	if !lOk {
+		lw = gateParts[0]
+	}
+	if !rOk {
+		rw = gateParts[2]
+	}
+	return 0, lw, rw
+}
+
+func getGate(w string, strs []string) string {
+	for _, str := range strs {
+		parts := strings.Split(str, " -> ")
+		gate, wire := parts[0], parts[1]
+		if w == wire {
+			return gate
+		}
+	}
+	return ""
 }
 
 func part1(strs []string) int {
 	wires := map[string]Wire{}
-	for _, str := range strs {
-		parts := strings.Split(str, " -> ")
-		gate, wire := parts[0], parts[1]
-		val, ok := processGate(gate, wires)
-		if ok {
-			wires[wire] = Wire{
+	stack := &Stack{"a"}
+	for !stack.IsEmpty() {
+		w := stack.Top()
+		var gate string
+		if wire, ok := wires[w]; ok {
+			gate = wire.Gate
+		} else {
+			gate = getGate(w, strs)
+		}
+		if gate == "" {
+			fmt.Printf("could not find gate for %s\n", w)
+			return -1
+		}
+		val, l, r := processGate(gate, wires)
+		if l == "" && r == "" {
+			wires[w] = Wire{
 				Val:      val,
 				Gate:     gate,
 				Resolved: true,
 			}
+			stack.Pop()
 		} else {
-			wires[wire] = Wire{
+			wires[w] = Wire{
+				Val:      0,
 				Gate:     gate,
 				Resolved: false,
+			}
+			if l != "" {
+				stack.Push(l)
+			}
+			if r != "" {
+				stack.Push(r)
 			}
 		}
 	}
@@ -81,7 +151,13 @@ func part1(strs []string) int {
 }
 
 func part2(strs []string) int {
-	return 0
+	for i, str := range strs {
+		if strings.Contains(str, " -> b") {
+			strs[i] = fmt.Sprintf("%d -> b", 3176)
+			break
+		}
+	}
+	return part1(strs)
 }
 
 func main() {
