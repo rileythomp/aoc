ad68305b7b339c5d56a5fcb03e34cac76ed5f13d
Riley Thompson <riley.thompson@flybits.com>
2021-12-13 16:29:11 -0500

    improve day13

--- solutions/2021/day13/main.go
+++ solutions/2021/day13/main.go
@@ -63,6 +63,57 @@ func part1(strs []string) int {
 }
 
 func part2(strs []string) int {
+	points := [][2]int{}
+	for _, str := range strs {
+		if str == "" {
+			continue
+		}
+		if strings.Contains(str, "fold") {
+			break
+		}
+		xy := strings.Split(str, ",")
+		x, _ := strconv.Atoi(xy[0])
+		y, _ := strconv.Atoi(xy[1])
+		points = append(points, [2]int{x, y})
+	}
+	for _, str := range strs {
+		if !strings.Contains(str, "fold") {
+			continue
+		}
+		dirval := strings.Split(strings.Fields(str)[2], "=")
+		dir, val := dirval[0], dirval[1]
+		num, _ := strconv.Atoi(val)
+		for i, p := range points {
+			if dir == "y" && p[1] > num {
+				points[i] = [2]int{p[0], 2*num - p[1]}
+			} else if dir == "x" && p[0] > num {
+				points[i] = [2]int{2*num - p[0], p[1]}
+			}
+		}
+	}
+	boardstr := ""
+	for y := 0; y < 7; y++ {
+		for x := 0; x < 159; x++ {
+			broke := false
+			for _, p := range points {
+				if p[0] == x && p[1] == y {
+					boardstr += "#"
+					broke = true
+					break
+				}
+			}
+			if !broke {
+				boardstr += " "
+			}
+			boardstr += " "
+		}
+		boardstr += "\n"
+	}
+	fmt.Println(boardstr)
+	return 0
+}
+
+func part2v0(strs []string) int {
 	board := [2000][2000]int{}
 	for _, str := range strs {
 		if str == "" {
